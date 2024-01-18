03e5db9c8a732b726188154b73bb75d83805315d
sonal-chauhan <sonal.chauhan@grofers.com>
2024-01-18 12:10:44 +0530

    config path

--- 9/community/entrypoint.sh
+++ 9/community/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-export SONAR_JDBC_URL=$(yq .SONAR_JDBC_URL /configs/config.yaml )
-export SONAR_JDBC_USERNAME=$(yq .SONAR_JDBC_USERNAME /configs/config.yaml )
-export SONAR_JDBC_PASSWORD=$(yq .SONAR_JDBC_PASSWORD /configs/config.yaml )
+export SONAR_JDBC_URL=$(yq .SONAR_JDBC_URL /opt/sonarqube/configs/config.yaml )
+export SONAR_JDBC_USERNAME=$(yq .SONAR_JDBC_USERNAME /opt/sonarqube/configs/config.yaml )
+export SONAR_JDBC_PASSWORD=$(yq .SONAR_JDBC_PASSWORD /opt/sonarqube/configs/config.yaml )
 
 DEFAULT_CMD=('/opt/java/openjdk/bin/java' '-jar' 'lib/sonarqube.jar' '-Dsonar.log.console=true' "-Dsonar.jdbc.url=${SONAR_JDBC_URL}" "-Dsonar.jdbc.username=${SONAR_JDBC_USERNAME}" "-Dsonar.jdbc.password=${SONAR_JDBC_PASSWORD}")
 
